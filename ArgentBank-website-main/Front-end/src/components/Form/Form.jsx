ce3ff05363b7ec2acbd7b75e8cca443f473bb720
Titouan2b <titouan2bsur@gmail.com>
2024-01-08 05:49:24 +0100

    toggle visibility login, logout and firstname in progress

--- ArgentBank-website-main/Front-end/src/components/Form/Form.jsx
+++ ArgentBank-website-main/Front-end/src/components/Form/Form.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import "./form.scss"
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { useDispatch } from "react-redux"
 import { useNavigate } from 'react-router-dom'
+import { loginUser } from "../../redux/reducers/authSlice"
 
 export default function Form() {
   const [userName, setUserName] = useState("") 
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  // const [data, setData] = useState()
-
-  async function login(event, name, password){
+  
+  async function edit(event, name, password){
     event.preventDefault()
     console.log(name)
     try{
@@ -19,14 +19,14 @@ export default function Form() {
         email: name,
         password: password
       }
-      const loginDataJson = JSON.stringify(loginData)
-      const sendLogin = await fetch("http://localhost:3001/api/v1/user/login",{
-        method: "POST",
-        headers: {
-          "Content-Type" : "application/json"
-        },
-        body: loginDataJson
-      })
+    const loginDataJson = JSON.stringify(loginData)
+    const sendLogin = await fetch("http://localhost:3001/api/v1/user/login",{
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: loginDataJson
+    })
       
       if(!sendLogin.ok){
         const sendLoginError = await sendLogin.json()
@@ -36,25 +36,23 @@ export default function Form() {
       const sendLoginResponse = await sendLogin.json()
       const token = sendLoginResponse.body.token
       console.log(token)
-      // dispatch(login({token}))
+      dispatch(loginUser({token}))
       navigate("/user")
-      // .then((response) => response.json())
-      // .then((data) => {
-      //   setData(data)
-      // })
-      // .catch((error) => {
-      //   console.log(error)
-      // })
     }catch(error){
       console.log(error.message)
     }
   }
 
+  const toggleVisibility = ({toggleVisibilityLogin}) => {
+
+  }
+
+
   return (
       <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
-          <form onSubmit={(event) => login(event, userName, password)}>
+          <form onSubmit={(event) => edit(event, userName, password)}>
 
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
@@ -68,7 +66,7 @@ export default function Form() {
               <input type="checkbox" id="remember-me" /><label htmlFor="remember-me"
                 >Remember me</label>
             </div>
-            <button type='submit' className="sign-in-button">Sign In</button>
+            <button type='submit' className="sign-in-button" onClick={toggleVisibility}>Sign In</button>
           </form>
         </section>
   )
--- ArgentBank-website-main/Front-end/src/components/Header/Header.jsx
+++ ArgentBank-website-main/Front-end/src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.scss"
 import ArgentBankLogo from "./../../assets/img/argentBankLogo.png"
 import { Link } from 'react-router-dom'
 
 
 export default function Header() {
+    const [login, setLogin] = useState(true)
+    const [logout, setLogout] = useState(false)
+
+    const toggleVisibility = () => {
+        setLogout(!login)
+        setLogin(!logout)
+    }
+
+
   return (
     <nav className="main-nav">
         <Link className="main-nav-logo" to="/">
@@ -16,11 +25,12 @@ export default function Header() {
             <h1 className="sr-only">Argent Bank</h1>
         </Link>
         <div>
-            <Link className="main-nav-item login" to="/sign-in">
+        {login && <Link className="main-nav-item login" to="/sign-in" onClick={toggleVisibility} >
                 <i className="fa fa-user-circle"></i>
-                Sign In
-            </Link>
-            <Link className='main-nav-item logout' to="/">Logout</Link>
+                <p>Sign In</p>
+            </Link>}
+            {logout && <Link className='main-nav-item logout' to="/" onClick={toggleVisibility}><p>Logout</p></Link>}
+            {logout && <p>{firstName}</p>}
         </div>
     </nav>
   )
