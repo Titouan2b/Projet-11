ffcb946e47d4bf7c558d49f5abfe260c313d5c9a
Titouan2b <titouan2bsur@gmail.com>
2024-01-10 03:37:12 +0100

    security token and front awesome

--- ArgentBank-website-main/Front-end/src/App.jsx
+++ ArgentBank-website-main/Front-end/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import './App.scss'
 import Home from './pages/Home/home';
 import User from './pages/User/User';
@@ -8,13 +8,15 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 function App() {
+  const token = useSelector((state) => state.auth.token)
+
   return (
     <BrowserRouter>
     <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/user" element={<User/>} />
+        <Route path="/user" element={token ? <User/> : <Navigate to="/sign-in"/>} />
       </Routes>
     <Footer/>
     </BrowserRouter>
